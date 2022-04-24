ccbbee5ce023527122b77549085d5d84418d3e5e
Bruno Torres <bruno_augusto_torres@yahoo.com.br>
2022-04-24 19:35:22 -0300

    Inicio do Cadastro de Usuários

--- sakila/src/main/java/br/com/sakila/controller/CadastroController.java
+++ sakila/src/main/java/br/com/sakila/controller/CadastroController.java
@@ -1,10 +1,14 @@
 package br.com.sakila.controller;
 
 import java.util.Arrays;
+import java.util.Random;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -24,6 +28,8 @@ public class CadastroController {
 	@Autowired
 	private Mailer mailer;
 	
+
+	
 	@GetMapping({"/cadastro/recuperar_senha"})
     public ModelAndView recuperarSenha(UsuarioModel usuario) { 	
 		ModelAndView mv = new ModelAndView("cadastro/recuperar_senha");
@@ -36,12 +42,17 @@ public class CadastroController {
 	
 		ModelAndView mv = new ModelAndView("cadastro/recuperar_senha");
 			
-		usuario = this.usuarioRepository.getOneByCpf("11111111111");
+		usuario = this.usuarioRepository.getOneByCpf(usuario.getCpf());
 		
+		String novaSenha = getRandomPass(); //gera uma senha randomica de 8 digitos
 		
+		BCryptPasswordEncoder b = new BCryptPasswordEncoder();
+		
+		usuarioRepository.alterarSenha(b.encode(novaSenha),usuario.getCpf());
+		        
 				this.mailer.enviar(new Mensagem("sakila - Etejk <noreplysakila@gmail.com>", 
 				Arrays.asList("Recuperação de senha <"+ usuario.getEmail() +">")
-				, "Recuperação de senha", "Olá! \n\n Sua nova senha é:  " + "novaSenha"  ));
+				, "Recuperação de senha", "Olá! \n\n Sua nova senha é:  " + novaSenha  ));
 				
 				model.addAttribute("mensagem", "Senha enviada com sucesso para o email cadastrado.");
 		
@@ -50,6 +61,45 @@ public class CadastroController {
         return mv;
     	    	
     }
+	
+	
+	
+	@GetMapping(value="/administracao/listar_usuarios")
+    public ModelAndView ListarUsuarios(ModelMap model) { 	
+		//Lista todos os usuários
+		ModelAndView mv = new ModelAndView("/administracao/listar_usuarios");		
+
+		
+		model.addAttribute("usuarios", this.usuarioRepository.findAll());
+		
+        return mv;  	
+    	    	
+    }
+	@GetMapping(value="/administracao/cadastrar_usuario")
+    public ModelAndView CadastrarUsuario(UsuarioModel usuario) { 	 
+		ModelAndView mv = new ModelAndView("/administracao/cadastrar_usuario");
+		
+        return mv;  	
+    	    	
+    }
+	
+	
+	
+	
+	
+	public String getRandomPass() {
+		char[] chart ={'0','1','2','3','4','5','6','7','8','9','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'};
+
+		char[] senha= new char[8];
+
+		int chartLenght = chart.length;
+		Random rdm = new Random();
+
+		for (int x=0; x<8; x++)
+		senha[x] = chart[rdm.nextInt(chartLenght)];
+
+		return new String(senha);
+	}
 
 
 }
